e0b85c12967d54ddb152a37c9575eebb725b350c
Rodrigo Volpe Battistin <rv.battistin@usp.br>
2022-03-24 07:05:11 -0300

    Rebase repository
    Overwrite attacker.c and defender.c
    
    Signed-off-by: Rodrigo Volpe Battistin <rv.battistin@usp.br>

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -13,17 +15,86 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                         PRIVATE VARIABLES                                  */
+/*----------------------------------------------------------------------------*/
+
+enum AttackState{START, DISTRACT, GO_TO_CENTER, SPRINT};
+static enum AttackState state = START;
+
+static size_t height_estimate; // Either height or (height - 1)
+
+static direction_t current_direction;
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  switch (state) {
+    case START :
+      srand((int) time(NULL)); // Randomizes seed
+      height_estimate = attacker_position.i * 2;
+
+      // Randomly chooses between going UP or DOWN
+      if (rand() <= RAND_MAX / 2)
+        current_direction = DIR_UP;
+      else
+        current_direction = DIR_DOWN;
+      
+      state = DISTRACT;
+      break;
+
+    case DISTRACT :
+      /* Keep going until you reach an edge of the field */
+      if (attacker_position.i == 1) { // Top of the field
+        current_direction = DIR_DOWN_RIGHT;
+        state = GO_TO_CENTER;
+      }
+
+      if (attacker_position.i >= height_estimate - 2) { // Bottom of the field
+        current_direction = DIR_UP_RIGHT;
+        state = GO_TO_CENTER;
+      }
+      break;
+
+    case GO_TO_CENTER :
+      /* Keep going until you are close to the center */
+      if (attacker_position.i == height_estimate / 2) {
+        size_t defender_i_at_spy = get_spy_position(defender_spy).i;
+
+        if (attacker_position.i > defender_i_at_spy) {
+          /* The attacker is below the defender in the field,
+           * so the attacker will sprint to the bottom corner
+           */
+          current_direction = DIR_DOWN_RIGHT;
+        }
+
+        else if (attacker_position.i < defender_i_at_spy) {
+          /* The attacker is above the defender in the field,
+           * so the attacker will sprint to top corner
+           */
+          current_direction = DIR_UP_RIGHT;
+        }
+
+        state = SPRINT;
+      }
+      break;
+
+    case SPRINT :
+      /* Go to one of the corners.
+       * If you reach a wall, just move straight ahead
+       */
+      if (attacker_position.i == 1 ||
+          attacker_position.i >= height_estimate - 2)
+      {
+        current_direction = DIR_RIGHT;
+      }
+      break;
+  }
+
+  return current_direction;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -13,17 +13,109 @@
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
+/*                         PRIVATE VARIABLES                                  */
+/*----------------------------------------------------------------------------*/
+
+enum DefenseState {START, ADVANCE, RETREAT, HOLD_GROUND, PATROL};
+static enum DefenseState state = START;
+
+static size_t height_estimate; // Either height or (height - 1)
+static size_t width; // Exactly the field width
+
+static direction_t current_direction;
+
+/*----------------------------------------------------------------------------*/
+/*                          PRIVATE FUNCTIONS HEADERS                         */
+/*----------------------------------------------------------------------------*/
+direction_t reverse_direction(direction_t current_direction);
+
+/*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  switch (state) {
+    case START :
+      height_estimate = defender_position.i * 2;
+      width = defender_position.j + 2;
+
+      current_direction = DIR_LEFT;
+      state = ADVANCE;
+      break;
+
+    case ADVANCE :
+      /* Go forward until you reach the center, then Spy */
+      if (defender_position.j == width / 2) {
+        size_t attacker_i_at_spy = get_spy_position(attacker_spy).i;
+
+        if (attacker_i_at_spy > defender_position.i) {
+          /* The attacker is below the defender in the field,
+           * so the defender will chase it that way
+           */
+          current_direction = DIR_DOWN_RIGHT;
+        }
+
+        else if (attacker_i_at_spy < defender_position.i) {
+          /* The attacker is above the defender in the field,
+           * so the defender will chase it that way
+           */
+          current_direction = DIR_UP_RIGHT;
+        }
+
+        else {
+          /* The attacker is coming from the centre line,
+           * so the defender will retreat in the same line
+           */
+          current_direction = DIR_RIGHT;
+        }
+
+        state = RETREAT;
+      }
+      break;
+
+    case RETREAT :
+      /* Retreat in the direction of the attacker, to try to catch it.
+       * When you reach the second to last walkable column,
+       * start patrolling or hold your ground
+       */
+      if (defender_position.j == width - 3) {
+        if (current_direction.i == 0) {
+          current_direction = DIR_LEFT;
+          state = HOLD_GROUND;
+        }
+        else {
+          current_direction = (direction_t){current_direction.i, 0};
+          state = PATROL;
+        }
+      }
+      break;
+
+    case HOLD_GROUND :
+      /* Keep going back and forth */
+      current_direction = reverse_direction(current_direction);
+      break;
+
+    case PATROL : 
+        /* Keep going up and down as much as you can */
+        if (defender_position.i <= 2 ||
+            defender_position.i >= height_estimate - 2)
+        {
+          current_direction = reverse_direction(current_direction);
+        }
+        break;
+    }
+
+  return current_direction;
+}
+
+/*----------------------------------------------------------------------------*/
+/*                             PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+direction_t reverse_direction(direction_t current_direction) {
+  return (direction_t){- current_direction.i, - current_direction.j};
 }
 
 /*----------------------------------------------------------------------------*/
